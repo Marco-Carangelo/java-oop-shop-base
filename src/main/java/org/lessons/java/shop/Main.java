fbf75a2dcc2c775093b869630ddbd7a79367659e
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-12 15:26:26 +0200

    add: created main method variables and acquired ther values

--- src/main/java/org/lessons/java/shop/Main.java
+++ src/main/java/org/lessons/java/shop/Main.java
@@ -1,10 +1,33 @@
 package org.lessons.java.shop;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//Dichiariamo delle variabili che verranno usate per acquisire i dati dei prodotti da tastiera e per popolare gli attributi degli oggetti
+		
+		String userProdName;
+		String userProdInfo;
+		float userProdPrice;
+		int userProdIva;
+		
+		//Acquisiamo i valori delle variabili
+		
+		System.out.println("Inserisci il nome del prodotto: ");
+		userProdName= scanner.nextLine();
+		System.out.println("Inserisci la descrizione del prodotto: ");
+		userProdInfo= scanner.nextLine();
+		System.out.println("Inserisci il prezzo del prodotto, non comprensivo di IVA: ");
+		userProdPrice= scanner.nextFloat();
+		System.out.println("Inserisci il valore dell'Iva da applicare sul prodotto: ");
+		userProdIva= scanner.nextInt();
+		
+		
+		scanner.close();
 	}
 
 }
