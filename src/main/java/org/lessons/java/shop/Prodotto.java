7fe01a7a70c5a24aa7f5b151fcbb9f98fc725e31
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-12 14:18:46 +0200

    add: Prodotto class and it's attributes

---
+++ src/main/java/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,17 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+	
+	//Dichiarazione degli attributi della classe
+	
+	int prodCode;
+	String prodName;
+	String prodInfo;
+	float prodPrice;
+	float prodIva;
+	
+	
+	
+	
+
+}
